b67fc9a84db7bda2445a564f8b682da001c16c18
Romain Felgines <romain.felgines@gmail.com>
2021-11-15 22:22:17 -0500

    fixing bugs

--- app/src/main/java/com/example/sharetovers/MainActivity.kt
+++ app/src/main/java/com/example/sharetovers/MainActivity.kt
@@ -8,8 +8,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    String s1[], s2[]
-    int images[] = { R.drawable.Asian;R.drawable.French;R.drawable.Packaged;R.drawable.Fruits;R.drawable.Indian;R.drawable.Asian;R.drawable.Pastries;R.drawable.Pizza;R.drawable.Desert;R.drawable.Mediteranean;R.drawable.Pasta}
+    var String s1[], s2[]
+    var int images[] = { R.drawable.Asian;R.drawable.French;R.drawable.Packaged;R.drawable.Fruits;R.drawable.Indian;R.drawable.Asian;R.drawable.Pastries;R.drawable.Pizza;R.drawable.Desert;R.drawable.Mediteranean;R.drawable.Pasta}
     RecyclerView recyclerView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         // val navController = findNavController(R.id.fragmentContainerView)
-
         // bottomNavigationView.setupWithNavController(navController)
 
         s1 = getResources().getStringArray(R.array.food_types)
