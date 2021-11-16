08927af72d8558335e6699f45cc72bfd02c487e8
Romain Felgines <romain.felgines@gmail.com>
2021-11-15 21:18:55 -0500

    Parite Romain
    RecyclerView non fonctionnel

---
+++ app/src/main/java/com/example/sharetovers/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.sharetovers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    String s1[], s2[]
+    int images[] = { R.drawable.Asian;R.drawable.French;R.drawable.Packaged;R.drawable.Fruits;R.drawable.Indian;R.drawable.Asian;R.drawable.Pastries;R.drawable.Pizza;R.drawable.Desert;R.drawable.Mediteranean;R.drawable.Pasta}
+    RecyclerView recyclerView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        // val navController = findNavController(R.id.fragmentContainerView)
+
+        // bottomNavigationView.setupWithNavController(navController)
+
+        s1 = getResources().getStringArray(R.array.food_types)
+        RecyclerView = findViewById<>(R.id.recyclerView)
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/sharetovers/MyViewHolder.kt
@@ -0,0 +1,5 @@
+package com.example.sharetovers
+
+class MyViewHolder {
+
+}
